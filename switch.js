7276a365745ecb307d169c7421c02fd20b043b60
ymtky <tybjea1348@gmail.com>
2019-03-02 14:27:19 +0900

    add minimum script

---
+++ switch.js
@@ -0,0 +1,43 @@
+(async() => {
+    targetTask = process.argv[2]
+    const tasks = require('./tasks.json')
+    if (!(targetTask in tasks)) {
+        console.log(`${targetTask} is not found`)
+        process.exit(1)
+    }
+    task = tasks[targetTask]
+
+    const execSync = require('child_process').execSync
+    const runnningApps = execSync('ps aux | grep -o "/Applications/[^/]*\.app/" | sort | uniq | sed "s/^\\/Applications\\/\\(.*\\).app\\//\\1/g"')
+        .toString()
+        .trim() 
+        .split("\n")
+    const ignore_apps = require('./ignore.json')
+    for (app of runnningApps) {
+        if (!(ignore_apps.includes(app))) {
+            try {
+                execSync(`killall "${app}"`)
+            } catch {
+                console.log(`failed to close ${app}`)
+            }
+        }
+    }
+
+    await sleep(1000)
+
+    for (app of task.apps) {
+        try {
+            execSync(`open -a '${app.name}' ${app.params.join(' ')}`)
+        } catch {
+            console.log(`failed to open ${app.name}`)
+        }
+    }
+})()
+
+function sleep(time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, time)
+    })
+}
